deb7f87e1910c2b617799a415b1f0687c2fee07a
Subhankar Saha <64220633+fl-Subhankar@users.noreply.github.com>
2020-06-13 22:11:18 +0530

    Update CoffeeMachine.java

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,206 +1,181 @@
-//package com.subhankar;
 package machine;
 
 import java.util.Scanner;
 
 public class CoffeeMachine {
-
-	// Methods : printStats(water, milk, coffee, cups, money);
-	public static void printStats(int water, int milk, int coffee, int cups, int money) {
-		System.out.println("The coffee machine has:\n" + water + " of water\n" + milk + " of milk\n" + coffee
-				+ " of coffee beans\n" + cups + " of disposable cups\n" + "$" + money + " of money\n");
-	}
-
-	// Method : haveResource();
-	public static void haveResource() {
-		System.out.println("I have enough resources, making you a coffee!");
-	}
-
-	// Methods : noWater();
-	public static void noWater() {
-		System.out.println("Sorry, not enough water!");
-	}
-
-	// Methods : noMilk();
-	public static void noMilk() {
-		System.out.println("Sorry, not enough milk!");
-	}
-
-	// Methods : noCoffee();
-	public static void noCoffee() {
-		System.out.println("Sorry, not enough water!");
-	}
-
-	// Methods : noCups();
-	public static void noCups() {
-		System.out.println("Sorry, not enough cups!");
-	}
-
-	// Methods : Main ; Starts here
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Ingredients stock = new Ingredients(400, 540, 120, 9, 550);
+		Ingredients espresso = new Ingredients(250, 0, 16, 1, 4);
+		Ingredients latte = new Ingredients(350, 75, 20, 1, 7);
+		Ingredients cappuccino = new Ingredients(200, 100, 12, 1, 6);
 
-		Scanner sc = new Scanner(System.in);
-
-		// Initialization
-		// Stock of materials :
-		int water = 400;
-		int milk = 540;
-		int coffee = 120;
-		int cups = 9;
-		int money = 550;
-
-		// For Espresso :
-		int esWater = 250;
-		int esCoffee = 16;
-		int esCups = 1;
-		int esMoney = 4;
-
-		// For Latte :
-		int ltWater = 350;
-		int ltMilk = 75;
-		int ltCoffee = 20;
-		int ltCups = 1;
-		int ltMoney = 7;
-
-		// For Cappuccino :
-		int cpWater = 200;
-		int cpMilk = 100;
-		int cpCoffee = 12;
-		int cpCups = 1;
-		int cpMoney = 6;
-
-		// Boolean :
 		boolean exitLoop = false;
-
-		// Print machine statistic :
-		// printStats(water, milk, coffee, cups, money);
-
-		// Option Loop :
 		while (exitLoop == false) {
-			// Option starting bar :
 			System.out.println("Write action (buy, fill, take, remaining, exit): ");
 
-			// Option Case :
-			switch (sc.next()) {
-
+			switch (scanner.next()) {
 			case "buy": {
 				System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-				switch (sc.next()) {
-
-				// Option : 1 - Espresso :
-				case "1": {
-					if (water >= esWater && coffee >= esCoffee && cups >= esCups) {
-						water -= esWater;
-						coffee -= esCoffee;
-						cups -= esCups;
-						money += esMoney;
-						haveResource();
-					} else {
-						if (water < esWater) {
-							noWater();
-						}
-						if (coffee < esCoffee) {
-							noCoffee();
-						}
-						if (cups < esCups) {
-							noCups();
-						}
-					}
+				switch (scanner.next()) {
+				case "1":
+					Operation.makeCoffee(stock, espresso);
+					break;
+				case "2":
+					Operation.makeCoffee(stock, latte);
+					break;
+				case "3":
+					Operation.makeCoffee(stock, cappuccino);
+					break;
+				case "back" :
 					break;
 				}
-
-				// Option : 2 - latte :
-				case "2": {
-					if (water >= ltWater && milk >= ltMilk && coffee >= ltCoffee && cups >= ltCups) {
-						water -= ltWater;
-						milk -= ltMilk;
-						coffee -= ltCoffee;
-						cups -= ltCups;
-						money += ltMoney;
-						haveResource();
-					} else {
-						if (water < ltWater) {
-							noWater();
-						}
-						if (milk < ltMilk) {
-							noMilk();
-						}
-						if (coffee < ltCoffee) {
-							noCoffee();
-						}
-						if (cups < ltCups) {
-							noCups();
-						}
-					}
-					break;
-				}
-
-				case "3": {
-					if (water >= cpWater && milk >= cpMilk && coffee >= cpCoffee && cups >= cpCups) {
-						water -= cpWater;
-						milk -= cpMilk;
-						coffee -= cpCoffee;
-						cups -= cpCups;
-						money += cpMoney;
-						haveResource();
-					} else {
-						if (water < cpWater) {
-							noWater();
-						}
-						if (milk < cpMilk) {
-							noMilk();
-						}
-						if (coffee < cpCoffee) {
-							noCoffee();
-						}
-						if (cups < cpCups) {
-							noCups();
-						}
-					}
-					break;
-				}
-				
-				case "back": {
-					break;
-				}
-			}break;
-		}
-				
-
-			case "fill": {
-				System.out.println("Write how many ml of water do you want to add: ");
-				water += sc.nextInt();
-
-				System.out.println("Write how many ml of milk do you want to add: ");
-				milk += sc.nextInt();
-
-				System.out.println("Write how many grams of coffee beans do you want to add: ");
-				coffee += sc.nextInt();
-
-				System.out.println("Write how many ml of water do you want to add: ");
-				cups += sc.nextInt();
-
-			}
-				break;
-
-			case "take": {
-				System.out.println("I gave you " + money);
-				money = 0;
-			}
-				break;
-
-			case "remaining": {
-				printStats(water, milk, coffee, cups, money);
 				break;
 			}
-
+			case "fill":
+				Operation.addIngredients(stock);
+				break;
+			case "take":
+				Operation.takeMoney(stock);
+				break;
+			case "remaining":
+				Operation.printStats(stock);
+				break;
 			case "exit": {
 				exitLoop = true;
 				break;
 			}
 			}
 		}
+		scanner.close();
+	}
+}
 
-		sc.close();
+class Ingredients {
+	int water;
+	int milk;
+	int coffee;
+	int cups;
+	int money;
+
+	public Ingredients(int water, int milk, int coffee, int cups, int money) {
+		this.water = water;
+		this.milk = milk;
+		this.coffee = coffee;
+		this.cups = cups;
+		this.money = money;
+	}
+
+// Getters & Setters:
+	public int getWater() {
+		return water;
+	}
+
+	public void setWater(int water) {
+		this.water = water;
+	}
+
+	public int getMilk() {
+		return milk;
+	}
+
+	public void setMilk(int milk) {
+		this.milk = milk;
+	}
+
+	public int getCoffee() {
+		return coffee;
+	}
+
+	public void setCoffee(int coffee) {
+		this.coffee = coffee;
+	}
+
+	public int getCups() {
+		return cups;
+	}
+
+	public void setCups(int cups) {
+		this.cups = cups;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+}
+
+class Operation {
+	Scanner scanner = new Scanner(System.in);
+
+	public static void makeCoffee(Ingredients stock, Ingredients coffeeType) {
+
+		boolean hasWater = false;
+		boolean hasMilk = false;
+		boolean hasCoffee = false;
+		boolean hasCups = false;
+
+		// Stock Check:
+		if (stock.water >= coffeeType.water)
+			hasWater = true;
+		else
+			System.out.println("Sorry, not enough water!");
+
+		if (stock.milk >= coffeeType.milk)
+			hasMilk = true;
+		else
+			System.out.println("Sorry, not enough milk!");
+
+		if (stock.coffee >= coffeeType.coffee)
+			hasCoffee = true;
+		else
+			System.out.println("Sorry, not enough coffee!");
+
+		if (stock.cups >= coffeeType.cups)
+			hasCups = true;
+		else
+			System.out.println("Sorry, not enough cups!");
+
+		// Final:
+		if (hasWater && hasMilk && hasCoffee && hasCups) {
+			stock.water -= coffeeType.water;
+			stock.milk -= coffeeType.milk;
+			stock.coffee -= coffeeType.coffee;
+			stock.cups -= coffeeType.cups;
+			stock.money += coffeeType.money;
+			System.out.println("I have enough resources, making you a coffee!");
+		}
+	}
+
+	@SuppressWarnings("resource")
+	public static void addIngredients(Ingredients stock) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Write how many ml of water do you want to add: ");
+		stock.water += scanner.nextInt();
+
+		System.out.println("Write how many ml of milk do you want to add: ");
+		stock.milk += scanner.nextInt();
+
+		System.out.println("Write how many grams of coffee beans do you want to add: ");
+		stock.coffee += scanner.nextInt();
+
+		System.out.println("Write how many numbers of cup do you want to add: ");
+		stock.cups += scanner.nextInt();
+
+	}
+
+	public static void printStats(Ingredients stats) {
+		System.out.println("The coffee machine has:\n" + stats.getWater() + " of water\n" + stats.getMilk()
+				+ " of milk\n" + stats.getCoffee() + " of coffee beans\n" + stats.getCups() + " of disposable cups\n"
+				 + stats.getMoney() + " of money\n");
+	}
+
+	public static void takeMoney(Ingredients stats) {
+		System.out.println("I gave you " + stats.getMoney());
+		stats.setMoney(0);
 	}
 }
